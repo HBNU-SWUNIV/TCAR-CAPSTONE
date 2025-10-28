04fda0a55ee218e2067a11b7ae435b67ecc7d2d5
서민재 <110338700+Vak-kas@users.noreply.github.com>
2025-10-29 04:13:46 +0900

    Upload VM Code
    it can controll k8s,.  and access websocket

---
+++ 003 Code/TCAR_VM/src/main/java/com/hanbat/dotcar/access/dto/AccessiblePodDto.java
@@ -0,0 +1,17 @@
+package com.hanbat.dotcar.access.dto;
+
+import com.hanbat.dotcar.kubernetes.domain.PodStatus;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AccessiblePodDto {
+    private String podName;
+    private String namespace;
+    private PodStatus status;
+    private String ingressUrl;
+    private String calledName;
+    private String accessType; // "owner" 또는 "guest"
+}
